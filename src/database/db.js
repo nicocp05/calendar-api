b51de1805d54dd89c78e8b916620f572af8e8d7a
NicoPcNueva <nico_cp05@hotmail.com>
2021-11-12 20:03:13 -0300

    Change dialect mysql

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,11 @@
 const Sequelize = require('sequelize');
 
-// const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-//     host: process.env.HOST,
-//     dialect: process.env.DB_DIALECT
-// });
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.HOST,
+    dialect: 'mysql'
+});
 
-const sequelize = new Sequelize( process.env.DATABASE_URL );
+// const sequelize = new Sequelize( process.env.DATABASE_URL );
 
 const connection = async () => {
     try {
