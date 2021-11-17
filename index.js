e8556a621d83c1f2524116e07e3b93d91a067dc0
NicoPcNueva <nico_cp05@hotmail.com>
2021-11-17 12:02:00 -0300

    add cross-env

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({
+    path: path.resolve(__dirname, '.env.' + process.env.NODE_ENV)
+});
 const cors = require('cors');
 const morgan = require('morgan');
 const { connection } = require('./src/database/db');
